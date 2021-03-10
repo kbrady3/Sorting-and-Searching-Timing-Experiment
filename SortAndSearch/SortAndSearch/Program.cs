346af06c649c164488ba8742b308a2336ae54f90
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-03-10 16:50:07 -0600

    Initialization

---
+++ SortAndSearch/SortAndSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SortAndSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
